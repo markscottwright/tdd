983c8d96920cfd74868dfc4b7059dbf997336edb
Mark Wright <markscottwright@gmail.com>
2015-07-11 11:26:17 -0400

    added first unit test, view, urlmapping

--- functional_tests.py
+++ functional_tests.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 import unittest
 
+
 class TestNewVisitor(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -20,22 +21,21 @@ class TestNewVisitor(unittest.TestCase):
         self.assertIn("To-Do", self.browser.title)
 
         # she is invited to add a todo item right away
-     
+
         # she types buy peacock feathers into a text box
 
         # When she hits enter, the page updates and now the page lsits
         # "1: Buy peacock feathers" as an item in a to-do list
 
-        # There is still a text box inviting her to enter another item.  She enters
-        # "Use peacock feathers to make a fly"
+        # There is still a text box inviting her to enter another item.  She
+        # enters "Use peacock feathers to make a fly"
 
         # The page updates again and now shows both items on her list
 
-        # the user notices that the site has generated a unique URL for her -- there
-        # is some explanatory text to that effect
+        # the user notices that the site has generated a unique URL for her --
+        # there is some explanatory text to that effect
 
         # She visits that URL - her todo list is still there
 
-if __name__=="__main__":
+if __name__ == "__main__":
     unittest.main()
-
