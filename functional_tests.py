f66bedcdc92d310f51cc95909c38b7c15701904b
Mark Wright <markscottwright@gmail.com>
2015-07-13 16:38:04 -0400

    Added workaround for selenium error

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 "Functional test script"
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -10,6 +11,9 @@ class TestNewVisitor(unittest.TestCase):
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
+        # this works around a bug that throws an exception in the django
+        # webserver
+        self.browser.refresh()
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -39,7 +43,8 @@ class TestNewVisitor(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item didn't appear in table"
         )
 
         # There is still a text box inviting her to add another item. She
