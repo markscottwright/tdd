eaa5846e28d756e5f686188205e7a4551159abd8
Mark Wright <markscottwright@gmail.com>
2015-07-15 07:33:39 -0400

    added model and migrations

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -29,3 +30,23 @@ class HomePageTest(TestCase):
             'home.html',
             {'new_item_text': 'A new list item'})
         self.assertEqual(expected_html, response.content.decode())
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(2, saved_items.count())
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertIn('The first (ever) list item', first_saved_item.text)
+        self.assertIn('Item the second', second_saved_item.text)
