0d0201c2269023414a0f8cc6d8e31fac1f453737
Mark Wright <markscottwright@gmail.com>
2015-07-24 08:11:16 -0400

    removed redundent tests

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -103,16 +103,6 @@ class ListViewTest(TestCase):
 class HomePageTest(TestCase):
     maxDiff=None
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEquals(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html', {'form': ItemForm()})
-        self.assertMultiLineEqual(response.content.decode(), expected_html)
-
     def test_displays_all_items(self):
         list_ = List.objects.create()
         Item.objects.create(text='itemy 1', list=list_)
